41870521c24801cf47be706b2d7ac91767a168b2
wemakefocus <lixiaoyu0575@qq.com>
2017-05-22 02:20:26 +0800

    first commit

---
+++ ng2Project/src/app/pages/new/new.component.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by xiaoyu on 17-5-14.
+ */
+import { Component } from  "@angular/core";
+
+@Component({
+  selector: "new",
+  template: `<strong>Test Page Content Here </strong>`,
+})
+export class NewComponent {
+  constructor(){}
+}
