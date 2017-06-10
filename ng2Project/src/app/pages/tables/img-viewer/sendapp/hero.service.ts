a79c8eb77b86385fc1df35ba07550b3442be4c1d
wemakefocus <lixiaoyu0575@qq.com>
2017-06-10 22:52:47 +0800

    changed getUser url

--- ng2Project/src/app/pages/tables/img-viewer/sendapp/hero.service.ts
+++ ng2Project/src/app/pages/tables/img-viewer/sendapp/hero.service.ts
@@ -8,7 +8,7 @@ import { Hero, Usercreds } from './hero';
 @Injectable()
 export class HeroService {
   private heroesUrl = 'api/heroes';
-  private emailUrl = 'http://202.117.54.45:3333/sendmail';
+  private emailUrl = 'http://59.110.52.133:3333/sendmail';
   private headers = new Headers({'Content-Type': 'application/json'});
   private emailheaders = new Headers({'Content-Type': 'application/json'});
 
